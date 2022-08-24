8944e3c5de8f21ee6788df31fe06fc03a737d524
Tau-Pi-2009 <108431572+Tau-Pi-2009@users.noreply.github.com>
2022-08-24 10:26:01 -0700

    Create

---
+++ console.js
@@ -0,0 +1,30 @@
+let admin = ['your id here']
+let pi = Math.PI;
+let tau = (Math.PI * 2);
+let e = Math.E;
+let sin = Math.sin;
+let cos = Math.cos;
+let tan = Math.tan;
+let date = Date()
+gClient.on("a", function (msg) {
+  let isAdmin = (admin.indexOf(msg.p._id) !== -1);
+  let args = msg.a.split("");
+  let cmd = args[0].toLowerCase();
+  let argcat = msg.a.substring(cmd.length).trim();
+  let userid = msg.p._id;
+  let usercolor = msg.p.color;
+  let username = msg.p.name;
+  if (cmd === `>`) {
+    if (isAdmin) {
+      try {
+        let out = eval(argcat);
+        if (out) {
+          MPP.chat.send(`✔ Console: ${out}`);
+        }
+      } catch (err) {
+        MPP.chat.send(`❌ Console: ${err}`);
+      };
+    };
+  };
+});
+ 
